1db41e64a3bd8180b4af98a2e1013b43d74447bc
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2021-09-07 15:36:58 +0530

    Updated 5

--- Assignment-1/5.sql
+++ Assignment-1/5.sql
@@ -1,3 +1,5 @@
+-- 1
+
 SELECT first_name,
 last_name,
 education.startyear,
@@ -6,3 +8,18 @@ FROM users,
 education
 WHERE (education.END_YEAR - education.startyear) <= 2 AND 
 users.userID = education.userID;
+
+
+-- 2 
+SELECT education.UserID,
+       users.first_name,
+       users.last_name,
+       COUNT(first_name) AS [Education Records]
+  FROM users,
+       education
+ WHERE users.userID = education.userID
+ GROUP BY first_name,
+          last_name
+HAVING COUNT(first_name) > 1 AND 
+       COUNT(last_name) > 1 AND 
+       COUNT(first_name) = COUNT(last_name);
